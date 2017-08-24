ed1cfb24560d7dffd7f7bcb3f710a6b00684463d
Jen Hamon <jhamon@gmail.com>
2017-08-23 20:16:29 -0700

    Blatant ripoff of credhub-cli config-related code

---
+++ config/config_test.go
@@ -0,0 +1,23 @@
+// +build !windows
+
+package config_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/jhamon/guac/config"
+)
+
+var _ = Describe("Config", func() {
+	var cfg config.Config
+
+	BeforeEach(func() {
+		cfg = config.Config{
+			TargetUrl:  "https://login.example.com",
+		}
+	})
+
+	It("places the config file in .uaa in the home directory", func() {
+		Expect(config.ConfigPath()).To(HaveSuffix(`/.uaa/config.json`))
+	})
+})
