9a70194e8f4241775f818571d81bd2a08b977b38
Jen Hamon <jhamon@gmail.com>
2017-09-09 23:09:39 -0700

    Add AuthcodeClientImpersonator

---
+++ cli/authcode_client_impersonator.go
@@ -0,0 +1,110 @@
+package cli
+
+import (
+	"code.cloudfoundry.org/uaa-cli/uaa"
+	"code.cloudfoundry.org/uaa-cli/utils"
+	"fmt"
+	"net/http"
+	"net/url"
+	"os"
+)
+
+type AuthcodeClientImpersonator struct {
+	httpClient         *http.Client
+	config             uaa.Config
+	ClientId           string
+	ClientSecret       string
+	TokenFormat        string
+	Scope              string
+	UaaBaseUrl         string
+	Port               int
+	Log                utils.Logger
+	AuthCallbackServer CallbackServer
+	BrowserLauncher    func(string) error
+	done               chan uaa.TokenResponse
+}
+
+const authcodeCallbackHTML = `<body>
+	<h1>Authorization Code Grant: Success</h1>
+	<p>The UAA redirected you to this page with an authorization code.</p>
+	<p>The CLI will exchange this code for an access token. You may close this window.</p>
+</body>`
+
+func NewAuthcodeClientImpersonator(
+	httpClient *http.Client,
+	config uaa.Config,
+	clientId,
+	clientSecret,
+	uaaBaseUrl,
+	tokenFormat,
+	scope string,
+	port int,
+	log utils.Logger,
+	launcher func(string) error) AuthcodeClientImpersonator {
+
+	impersonator := AuthcodeClientImpersonator{
+		httpClient:      httpClient,
+		config:          config,
+		ClientId:        clientId,
+		ClientSecret:    clientSecret,
+		UaaBaseUrl:      uaaBaseUrl,
+		TokenFormat:     tokenFormat,
+		Scope:           scope,
+		Port:            port,
+		BrowserLauncher: launcher,
+		Log:             log,
+		done:            make(chan uaa.TokenResponse),
+	}
+
+	callbackServer := NewAuthCallbackServer(authcodeCallbackHTML, CallbackCSS, "", log, port)
+	callbackServer.SetHangupFunc(func(done chan url.Values, values url.Values) {
+		token := values.Get("code")
+		if token != "" {
+			done <- values
+		}
+	})
+	impersonator.AuthCallbackServer = callbackServer
+
+	return impersonator
+}
+
+func (aci AuthcodeClientImpersonator) Start() {
+	go func() {
+		urlValues := make(chan url.Values)
+		go aci.AuthCallbackServer.Start(urlValues)
+		values := <-urlValues
+		code := values.Get("code")
+		tokenRequester := uaa.AuthorizationCodeClient{ClientId: aci.ClientId, ClientSecret: aci.ClientSecret}
+		resp, err := tokenRequester.RequestToken(aci.httpClient, aci.config, uaa.TokenFormat(aci.TokenFormat), code, aci.redirectUri())
+		if err != nil {
+			fmt.Println(err.Error())
+			aci.Log.Error(err.Error())
+			os.Exit(1)
+		}
+		aci.Done() <- resp
+	}()
+}
+func (aci AuthcodeClientImpersonator) Authorize() {
+	requestValues := url.Values{}
+	requestValues.Add("response_type", "code")
+	requestValues.Add("client_id", aci.ClientId)
+	requestValues.Add("client_secret", aci.ClientSecret)
+	requestValues.Add("scope", aci.Scope)
+	requestValues.Add("redirect_uri", aci.redirectUri())
+
+	authUrl, err := utils.BuildUrl(aci.UaaBaseUrl, "/oauth/authorize")
+	if err != nil {
+		aci.Log.Error("Something went wrong while building the authorization URL.")
+		os.Exit(1)
+	}
+	authUrl.RawQuery = requestValues.Encode()
+
+	aci.Log.Info("Launching browser window to " + authUrl.String())
+	aci.BrowserLauncher(authUrl.String())
+}
+func (aci AuthcodeClientImpersonator) Done() chan uaa.TokenResponse {
+	return aci.done
+}
+func (aci AuthcodeClientImpersonator) redirectUri() string {
+	return fmt.Sprintf("http://localhost:%v", aci.Port)
+}
---
+++ cli/authcode_client_impersonator_test.go
@@ -0,0 +1,114 @@
+package cli_test
+
+import (
+	. "code.cloudfoundry.org/uaa-cli/cli"
+
+	"code.cloudfoundry.org/uaa-cli/uaa"
+	"code.cloudfoundry.org/uaa-cli/utils"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega/ghttp"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+)
+
+var _ = Describe("AuthcodeClientImpersonator", func() {
+	var (
+		impersonator AuthcodeClientImpersonator
+		logger       utils.Logger
+		httpClient   *http.Client
+		config       uaa.Config
+	)
+
+	BeforeEach(func() {
+		httpClient = &http.Client{}
+		config = uaa.NewConfig()
+		logger = utils.NewLogger(ioutil.Discard, ioutil.Discard, ioutil.Discard, ioutil.Discard)
+	})
+
+	Describe("NewAuthcodeClientImpersonator", func() {
+		BeforeEach(func() {
+			launcher := TestLauncher{}
+			impersonator = NewAuthcodeClientImpersonator(httpClient, config, "authcodeClientId", "authcodesecret", "http://uaa.com", "jwt", "openid", 8080, logger, launcher.Run)
+		})
+
+		Describe("configures an AuthCallbackListener", func() {
+			It("with appropriate static content", func() {
+				Expect(impersonator.AuthCallbackServer.CSS()).To(ContainSubstring("Source Sans Pro"))
+				Expect(impersonator.AuthCallbackServer.Html()).To(ContainSubstring("Authorization Code Grant: Success"))
+			})
+
+			It("with the desired port", func() {
+				Expect(impersonator.AuthCallbackServer.Port()).To(Equal(8080))
+			})
+
+			It("with its logger", func() {
+				Expect(impersonator.AuthCallbackServer.Log()).NotTo(Equal(utils.Logger{}))
+				Expect(impersonator.AuthCallbackServer.Log()).To(Equal(logger))
+			})
+
+			It("with hangup func that looks for code in query params", func() {
+				done := make(chan url.Values)
+
+				urlParams := url.Values{}
+				urlParams.Add("code", "56575db17b164e568668c0085ed14ae1")
+				go impersonator.AuthCallbackServer.Hangup(done, urlParams)
+
+				Expect(<-done).To(Equal(urlParams))
+			})
+		})
+	})
+
+	Describe("#Start", func() {
+		It("starts the AuthCallbackServer", func() {
+			httpClient := &http.Client{}
+			launcher := TestLauncher{}
+			uaaServer := NewServer()
+			config = uaa.NewConfigWithServerURL(uaaServer.URL())
+			uaaServer.RouteToHandler("POST", "/oauth/token", CombineHandlers(
+				VerifyRequest("POST", "/oauth/token"),
+				RespondWith(http.StatusOK, `{
+				  "access_token" : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ",
+				  "token_type" : "bearer",
+				  "expires_in" : 3000,
+				  "scope" : "openid",
+				  "jti" : "bc4885d950854fed9a938e96b13ca519"
+				}`),
+				VerifyFormKV("client_id", "authcodeId"),
+				VerifyFormKV("client_secret", "authcodesecret"),
+				VerifyFormKV("grant_type", "authorization_code"),
+				VerifyFormKV("token_format", "jwt"),
+				VerifyFormKV("response_type", "token"),
+				VerifyFormKV("code", "secretcode"),
+				VerifyFormKV("redirect_uri", "http://localhost:8080")),
+			)
+			impersonator = NewAuthcodeClientImpersonator(httpClient, config, "authcodeId", "authcodesecret", uaaServer.URL(), "jwt", "openid", 8080, logger, launcher.Run)
+
+			// Start the callback server
+			go impersonator.Start()
+
+			// Hit the callback server with an authcode
+			httpClient.Get("http://localhost:8080/?code=secretcode")
+
+			// The callback server should have exchanged the code for a token
+			tokenResponse := <-impersonator.Done()
+			Expect(tokenResponse.AccessToken).To(Equal("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ"))
+			Expect(tokenResponse.TokenType).To(Equal("bearer"))
+			Expect(tokenResponse.Scope).To(Equal("openid"))
+			Expect(tokenResponse.JTI).To(Equal("bc4885d950854fed9a938e96b13ca519"))
+			Expect(tokenResponse.ExpiresIn).To(Equal(int32(3000)))
+		})
+	})
+
+	Describe("#Authorize", func() {
+		It("launches a browser to the authorize page", func() {
+			launcher := TestLauncher{}
+			impersonator = NewAuthcodeClientImpersonator(httpClient, config, "authcodeId", "authcodesecret", "http://uaa.com", "jwt", "openid", 8080, logger, launcher.Run)
+
+			impersonator.Authorize()
+
+			Expect(launcher.TargetUrl).To(Equal("http://uaa.com/oauth/authorize?client_id=authcodeId&client_secret=authcodesecret&redirect_uri=http%3A%2F%2Flocalhost%3A8080&response_type=code&scope=openid"))
+		})
+	})
+})
