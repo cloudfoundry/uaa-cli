423d1a084006e0ac7b283252991275af4bf62dd4
Jen Hamon <jhamon@gmail.com>
2017-09-12 18:02:57 -0700

    Refactor error handling in get-client-credentials-token

--- cmd/get_client_credentials_token.go
+++ cmd/get_client_credentials_token.go
@@ -5,44 +5,52 @@ import (
 	"code.cloudfoundry.org/uaa-cli/help"
 	"code.cloudfoundry.org/uaa-cli/uaa"
 	"github.com/spf13/cobra"
-	"os"
+	"net/http"
+	"errors"
 )
+
+func GetClientCredentialsTokenValidations(cfg uaa.Config, args []string, clientSecret string) error {
+	if err := EnsureTargetInConfig(cfg); err != nil {
+		return err
+	}
+	if len(args) < 1 {
+		return MissingArgumentError("client_id")
+	}
+	if clientSecret == "" {
+		return MissingArgumentError("client_secret")
+	}
+	return validateTokenFormatError(tokenFormat)
+}
+
+func GetClientCredentialsTokenCmd(cfg uaa.Config, httpClient *http.Client, clientId, clientSecret string) error {
+	ccClient := uaa.ClientCredentialsClient{ClientId: clientId, ClientSecret: clientSecret}
+	tokenResponse, err := ccClient.RequestToken(httpClient, cfg, uaa.TokenFormat(tokenFormat))
+	if err != nil {
+		return errors.New("An error occurred while fetching token.")
+	}
+
+	activeContext := cfg.GetActiveContext()
+	activeContext.GrantType = uaa.CLIENT_CREDENTIALS
+	activeContext.ClientId = clientId
+	activeContext.TokenResponse = tokenResponse
+
+	cfg.AddContext(activeContext)
+	config.WriteConfig(cfg)
+	log.Info("Access token successfully fetched and added to context.")
+	return nil
+}
 
 var getClientCredentialsTokenCmd = &cobra.Command{
 	Use:   "get-client-credentials-token CLIENT_ID -s CLIENT_SECRET",
 	Short: "Obtain an access token using the client_credentials grant type",
 	Long:  help.ClientCredentials(),
-	Run: func(cmd *cobra.Command, args []string) {
-		ccClient := uaa.ClientCredentialsClient{ClientId: args[0], ClientSecret: clientSecret}
-		c := GetSavedConfig()
-		tokenResponse, err := ccClient.RequestToken(GetHttpClient(), c, uaa.TokenFormat(tokenFormat))
-		if err != nil {
-			log.Error("An error occurred while fetching token.")
-			TraceRetryMsg(c)
-			os.Exit(1)
-		}
-
-		activeContext := c.GetActiveContext()
-		activeContext.GrantType = uaa.CLIENT_CREDENTIALS
-		activeContext.ClientId = args[0]
-		activeContext.TokenResponse = tokenResponse
-
-		c.AddContext(activeContext)
-		config.WriteConfig(c)
-		log.Info("Access token successfully fetched and added to context.")
+	PreRun: func(cmd *cobra.Command, args []string) {
+		cfg := GetSavedConfig()
+		NotifyValidationErrors(GetClientCredentialsTokenValidations(cfg, args, clientSecret), cmd, log)
 	},
-	Args: func(cmd *cobra.Command, args []string) error {
-		EnsureTarget()
-
-		if len(args) < 1 {
-			MissingArgument("client_id", cmd)
-		}
-		if clientSecret == "" {
-			MissingArgument("client_secret", cmd)
-		}
-		validateTokenFormat(cmd, tokenFormat)
-
-		return nil
+	Run: func(cmd *cobra.Command, args []string) {
+		cfg := GetSavedConfig()
+		NotifyErrorsWithRetry(GetClientCredentialsTokenCmd(cfg, GetHttpClient(), args[0], clientSecret), cfg, log)
 	},
 }
 
