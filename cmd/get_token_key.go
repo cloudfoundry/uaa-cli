7552194af3927396ff3815d308934771b27b428d
Jen Hamon <jhamon@gmail.com>
2017-10-05 20:28:13 -0700

    Add token-key alias
    [#151690456] https://www.pivotaltracker.com/story/show/151690456

--- cmd/get_token_key.go
+++ cmd/get_token_key.go
@@ -7,13 +7,6 @@ import (
 	"code.cloudfoundry.org/uaa-cli/cli"
 )
 
-func GetTokenKeyValidations(cfg uaa.Config) error {
-	if err := EnsureTargetInConfig(cfg); err != nil {
-		return err
-	}
-	return nil
-}
-
 func GetTokenKeyCmd(client *http.Client, config uaa.Config) error {
 	key, err := uaa.TokenKey(client, config)
 
@@ -27,9 +20,10 @@ func GetTokenKeyCmd(client *http.Client, config uaa.Config) error {
 var getTokenKeyCmd = &cobra.Command{
 	Use:   "get-token-key",
 	Short: "View the key for validating UAA's JWT token signatures",
+	Aliases: []string{"token-key"},
 	PreRun: func(cmd *cobra.Command, args []string) {
 		cfg := GetSavedConfig()
-		NotifyValidationErrors(GetTokenKeyValidations(cfg), cmd, log)
+		NotifyValidationErrors(EnsureTargetInConfig(cfg), cmd, log)
 	},
 	Run: func(cmd *cobra.Command, args []string) {
 		NotifyErrorsWithRetry(GetTokenKeyCmd(GetHttpClient(), GetSavedConfig()), GetSavedConfig(), log)
