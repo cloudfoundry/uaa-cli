5f32a8e1692a5ec5b7622fe6c80ca71c33cef772
Jaskanwal Pawar <jpawar@pivotal.io>
2018-06-28 16:40:38 -0700

    Modified CI task to use make
    [#158611580] https://www.pivotaltracker.com/story/show/158611580
    
    Signed-off-by: Bruce Ricard <bricard@pivotal.io>

--- ci/tasks/run-tests/task.sh
+++ ci/tasks/run-tests/task.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+set -eux
+
 mkdir -p go/src/code.cloudfoundry.org/uaa-cli
 cp -R uaa-cli/* go/src/code.cloudfoundry.org/uaa-cli
 
 cd go/src/code.cloudfoundry.org/uaa-cli
 
-go get github.com/onsi/ginkgo/ginkgo
-ginkgo -v -r -randomizeAllSpecs -randomizeSuites
+make
 
