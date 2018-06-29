3b6d53f2915a554bcc5aa0bea5d01a0b6d6b56ad
Bruce Ricard <bricard@pivotal.io>
2018-06-29 12:55:43 -0700

    fix gopath

--- ci/tasks/run-tests/task.sh
+++ ci/tasks/run-tests/task.sh
@@ -4,8 +4,6 @@ set -eux
 
 mkdir -p go/src/code.cloudfoundry.org/uaa-cli
 cp -R uaa-cli/* go/src/code.cloudfoundry.org/uaa-cli
-
-cd go/src/code.cloudfoundry.org/uaa-cli
 export GOPATH="$(pwd)/go"
+cd go/src/code.cloudfoundry.org/uaa-cli
 make
-
