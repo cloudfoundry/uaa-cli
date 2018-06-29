da90ed09569c276cd9807db7709178f8d049a025
Bruce Ricard <bricard@pivotal.io>
2018-06-29 13:47:23 -0700

    Add GOPATH to PATH in CI task

--- ci/tasks/run-tests/task.sh
+++ ci/tasks/run-tests/task.sh
@@ -5,5 +5,6 @@ set -eux
 mkdir -p go/src/code.cloudfoundry.org/uaa-cli
 cp -R uaa-cli/* go/src/code.cloudfoundry.org/uaa-cli
 export GOPATH="$(pwd)/go"
+export PATH="${PATH}:${GOPATH}/bin"
 cd go/src/code.cloudfoundry.org/uaa-cli
 make
